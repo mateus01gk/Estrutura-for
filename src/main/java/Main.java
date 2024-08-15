50897cd557dc850007c285d983488c1acd79df6e
mateus01gk <169069087+mateus01gk@users.noreply.github.com>
2024-08-15 11:30:40 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+  //fazer um programa que lê um valor inteiro n e depois n números inteiros . ao final , mostra a soma dos n numeros lidos 
+
+for (int i = 0 ; i < 5 ;i++ ){
+  System.out.println(i);
+}
+for (int j = 4; j >= 0;j--){
+  System.out.println(j);
+}
+// for(incio; condição;incremento){  //comandos  
+//}
+
+
+sc.close();
+  }
+
+  
+}
